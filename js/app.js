2d5bb9afa9f9c50f155b7aa528e8858828582b0e
Sampreet <sam.learnnew@gmail.com>
2020-09-16 06:54:46 -0700

    Carousel implemented with Google Sheet Projects.

--- js/app.js
+++ js/app.js
@@ -18,20 +18,43 @@ $.ajax({ url: sheetAsJSON }).then((data) => {
 	});
 
 	console.log(' projects - ', projects);
-	renderProjects(projects);
+	renderProjectCarousel(projects);
+	renderAllProjects(projects);
 });
 
-const renderProjects = (projects) => {
-	console.log('Rendering projects...', projects);
+const renderProjectCarousel = (projects) => {
+	console.log('Rendering project carousel...', projects);
+
+	projects.forEach((project, index) => {
+		const itemActive = index === 0 ? ' active' : '';
+
+		const $li = `<li data-target="#carouselExampleCaptions" data-slide-to="${index}" class="active"></li>`;
+		$('.carousel-indicators').append($li);
+
+		const $carouselItem = $(`<div class="carousel-item${itemActive}">
+					<div><img src="${project.image}" alt="${project.title} picture" class="d-block w-100"></div>
+					<div class="carousel-caption">
+						<h5>${project.title}</h5>
+						<p>${project.description}</p>
+						<p class="project-view" id="project-view"><a href="${project.url}" target="_blank">View Here</a></p>
+					</div>
+					</div>`);
+		$('.carousel-inner').append($carouselItem);
+		console.log('Project Carousel rendered...');
+	});
+};
+
+const renderAllProjects = (projects) => {
+	console.log('Rendering all projects...', projects);
 	for (project of projects) {
 		const $article = $(`<article class="project-card">	
 						<div class="project-image"><img src="${project.image}" alt="${project.title} picture"></div>
 						<p class="project-title" id="project-title" >${project.title}</p>
 						<p class="project-description" id="project-description">${project.description}</p>
-						<p class="project-view" id="project-view"><a href="${project.url}">View Here</a></p>
+						<p class="project-view" id="project-view"><a href="${project.url}" target="_blank">View Here</a></p>
 					</article>`);
 		$('.projects-main').append($article);
-		console.log('Projects rendered...');
+		console.log('All Projects rendered...');
 	}
 };
 
@@ -45,9 +68,9 @@ $('form').on('submit', (event) => {
 		`Get In Touch - Full Name: ${fullName}, email: ${email}, message: "${message}"`
 	);
 
-	$('#contact-thanks').text(
-		'Thanks for reaching out! I shall get back to you in 1-2 days.'
-	);
+	$('#contact-thanks')
+		.text('Thanks for reaching out! I shall get back to you in 1-2 days.')
+		.css('display', 'block');
 
 	$(event.currentTarget).trigger('reset');
 });
